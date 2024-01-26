424b02301777c5c2dc770e2ae7189309e3271b76
Katherine Paucar <gaby2091@outlook.es>
2024-01-26 20:21:28 +0100

    add components + tailwindcss

---
+++ 1-vue-lab/to-do-app/components/List/TaskList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Título
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Contenido
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Estado
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Acciones
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  
+}>()
+</script>
---
+++ 1-vue-lab/to-do-app/components/ToDoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <NewTask/>
+  <TaskList/>
+</template>
+
+<script setup lang="ts">
+import TaskList from './List/TaskList.vue';
+
+import NewTask from './List/NewTask.vue';
+
+defineProps<{
+  
+}>()
+</script>
