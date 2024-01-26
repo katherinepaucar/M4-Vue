69e0cb0977dcc13d6851e2968e9075ceb3f708c9
Katherine Paucar <gaby2091@outlook.es>
2024-01-26 20:25:33 +0100

    update newTask

--- 1-vue-lab/to-do-app/components/List/NewTask.vue
+++ 1-vue-lab/to-do-app/components/List/NewTask.vue
@@ -1,17 +1,18 @@
 <template>
   <form class="max-w-sm mx-auto">
+    <h1>Añadir tareas</h1>
         <div class="mb-5">
             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
             <input type="title" id="title"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@flowbite.com" required>
+                placeholder="Título de la tarea" required>
         </div>
         <div class="mb-5">
 
-            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
             <textarea id="message" rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Write your thoughts here..."></textarea>
+                placeholder="Escribe la descripción de la tarea"></textarea>
         </div>
         <div class="flex items-start mb-5">
 
