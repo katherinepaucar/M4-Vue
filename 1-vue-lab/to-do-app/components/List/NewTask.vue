857ff361bf5bae3e395ac61c278c4ba3f4d41de3
Katherine Paucar <gaby2091@outlook.es>
2024-01-26 21:55:46 +0100

    Update NewTask.vue

--- 1-vue-lab/to-do-app/components/List/NewTask.vue
+++ 1-vue-lab/to-do-app/components/List/NewTask.vue
@@ -5,6 +5,7 @@
             <label for="title"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
             <input type="title" id="title"
                 v-model="title"
+                minlength="5"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Título de la tarea" required>
         </div>
@@ -13,6 +14,7 @@
             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
             <textarea id="message" rows="4"
             v-model="content"
+            minlength="5"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Escribe la descripción de la tarea"></textarea>
         </div>
@@ -37,7 +39,9 @@ const content = ref('');
 const taskService = useTask();
 
 const onSubmit = () => {
-    console.log('ddd')
+    if(!title && !content) {
+        return;
+    }
     taskService.addTask({
         title: title.value,
         content: content.value,
