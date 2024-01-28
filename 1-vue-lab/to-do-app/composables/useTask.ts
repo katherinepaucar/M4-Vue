6e95a4249dc960bca53123daf38194f5eaaee89b
Katherine Paucar <gaby2091@outlook.es>
2024-01-28 19:11:20 +0100

    update task

--- 1-vue-lab/to-do-app/composables/useTask.ts
+++ 1-vue-lab/to-do-app/composables/useTask.ts
@@ -1,27 +1,23 @@
-import {defineStore} from 'pinia';
-import type { TaskModel } from '~/types/task.model';
+import { defineStore } from "pinia";
+import type { TaskModel } from "~/types/task.model";
 const generateIdTask = () => {
-    const id = Math.random().toString(36).substring(2, 15);
-    return id;
-  }
-  
-export const useTask = defineStore('task', () => {
-    const listTask = ref<TaskModel[]>([]);
-    const addTask  = (task: TaskModel) => {
-        console.log('task', task);
-        task.id = generateIdTask();
-        listTask.value.push(task);
-        console.log('list', listTask.value);
-    }
-    const getList=() => {
-        return listTask;
-    }
+  const id = Math.random().toString(36).substring(2, 15);
+  return id;
+};
 
-    return {
-        listTask,
-        addTask
-    }
+export const useTask = defineStore("task", () => {
+  const listTask = ref<TaskModel[]>([]);
+  const addTask = (task: TaskModel) => {
+    task.id = generateIdTask();
+    listTask.value.push(task);
+  };
+  const deleteTask = (id: string) => {
+    listTask.value = listTask.value.filter((task) => task.id !== id);
+  };
 
-},
-
-)
+  return {
+    listTask,
+    addTask,
+    deleteTask,
+  };
+});
